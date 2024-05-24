61f68ff44655ca8f856df930b63e2e31c10cd8a6
MoodyEG <moodyeg2011@yahoo.com>
2024-05-25 01:05:27 +0300

    Fix pycode

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -63,7 +63,8 @@ def create_amenity():
         return make_response(jsonify(new_amenity.to_dict()), 201)
 
 
-@app_views.route('/amenities/<amenity_id>', strict_slashes=False, methods=['PUT'])
+@app_views.route('/amenities/<amenity_id>',
+                 strict_slashes=False, methods=['PUT'])
 def update_amenity(amenity_id):
     """Updates a amenity object with given keys and values
     """
